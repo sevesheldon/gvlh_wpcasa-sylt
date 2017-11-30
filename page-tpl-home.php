db75293b2cbd1c1c41e6dd56c35963ee6eec6849
Seve Sheldon <sevesheldon@gmail.com>
2017-11-29 16:21:38 -0800

    Move #home-cta-2 higher on the home page, alter the background color and change the max-width of parent div to give more prominance to the sponsors.

--- page-tpl-home.php
+++ page-tpl-home.php
@@ -13,13 +13,13 @@ get_header(); ?>
 	<?php get_template_part( 'template-parts/home', 'icon-links' ); ?>
 	
 	<?php get_template_part( 'template-parts/header', 'search' ); ?>
+
+	<?php get_template_part( 'template-parts/home', 'cta-2' ); ?>
 	
 	<?php get_template_part( 'template-parts/home', 'listings' ); ?>
 	
 	<?php get_template_part( 'template-parts/home', 'cta-1' ); ?>
 	
 	<?php get_template_part( 'template-parts/home', 'carousel' ); ?>
-	
-	<?php get_template_part( 'template-parts/home', 'cta-2' ); ?>
 
 <?php get_footer(); ?>
